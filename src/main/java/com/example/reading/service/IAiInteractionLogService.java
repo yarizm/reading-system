3b08eeb50bacd51b11b2bcd85f4896699b3aaa55
YARIZM <moriey757575@gmail.com>
2026-02-12 18:45:24 +0800

    11

---
+++ src/main/java/com/example/reading/service/IAiInteractionLogService.java
@@ -0,0 +1,16 @@
+package com.example.reading.service;
+
+import com.example.reading.entity.AiInteractionLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * AI功能使用记录表 服务类
+ * </p>
+ *
+ * @author CodingAssistant
+ * @since 2026-01-31
+ */
+public interface IAiInteractionLogService extends IService<AiInteractionLog> {
+
+}
